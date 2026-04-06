35e1478cbf941b09fac3b13753f5b6d8ddec6f2f
PIPALIYA_DIXIT <pipaliyadikshit@gmail.com>
2026-04-06 11:12:55 +0530

    Updated index and script file

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ console.log("hello every one")
 const helo = "Hello";
 
 console.log("this is a js")
+
+
+function name1()
+{
+    console.log("name is give")
+}
+
+function calculate()
+{
+    console.log("function is calculate");
+}
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function add(){
     console.log("add is called");
 }
+
+function name()
+{
+    console.log("name is added")
+}
